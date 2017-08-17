ddbdfb5320b09d8555d6abc059ff02631fb6d104
a170828 <acalvoe@externos.bankia.com>
2017-08-17 14:36:42 +0200

    Add & Create template recipe item

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe(
+      'A test Recipe',
+      'This is a simply a test',
+      'http://www.verkleedpret.com/wp-content/uploads/2017/04/Recipe_logo-1.jpeg'
+    ), new Recipe(
+      'A test Recipe',
+      'This is a simply a test',
+      'http://www.verkleedpret.com/wp-content/uploads/2017/04/Recipe_logo-1.jpeg'
+    ), new Recipe(
+      'A test Recipe',
+      'This is a simply a test',
+      'http://www.verkleedpret.com/wp-content/uploads/2017/04/Recipe_logo-1.jpeg'
+    )
+  ];
   constructor() { }
 
   ngOnInit() {
