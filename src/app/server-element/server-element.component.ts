f2f02e0f7c8e1bfeb49f42ee4d0914f431eea864
evuz <jesus_jgb_45@hotmail.com>
2017-08-18 14:34:03 +0200

    View encapsulation mode

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,12 +1,13 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
-  styleUrls: ['./server-element.component.css']
+  styleUrls: ['./server-element.component.css'],
+  encapsulation: ViewEncapsulation.Emulated // Native, None
 })
 export class ServerElementComponent implements OnInit {
-  @Input() private element: {type: string, name: string, content: string};
+  @Input() private element: { type: string, name: string, content: string };
 
   constructor() { }
 
