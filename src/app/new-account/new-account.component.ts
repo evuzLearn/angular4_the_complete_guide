bb80fe53d7f3a9da581940eab22c7c20fefcb044
evuz <jesus_jgb_45@hotmail.com>
2017-08-22 12:52:02 +0200

    Injecting Services into Services

--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -5,17 +5,14 @@ import { AccountsService } from '../accounts.service';
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
-  styleUrls: ['./new-account.component.css'],
-  providers: [LoggingService]
+  styleUrls: ['./new-account.component.css']
 })
 export class NewAccountComponent {
   constructor(
-    private loggingService: LoggingService,
     private accountsService: AccountsService
   ) { }
 
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountsService.addAcount(accountName, accountStatus);
-    this.loggingService.logStatusChange(accountStatus);
   }
 }
