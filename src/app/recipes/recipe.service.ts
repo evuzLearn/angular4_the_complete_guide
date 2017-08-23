8bb9b8c27d37e5534c5c4f17e860bf57813ba0dd
evuz <jesus_jgb_45@hotmail.com>
2017-08-23 09:26:18 +0200

    Adding Ingredients to Recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,5 +1,6 @@
 import { EventEmitter, Output } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
@@ -7,14 +8,22 @@ export class RecipeService {
     new Recipe(
       'A test Recipe',
       'This is a simply a test',
-      'http://www.verkleedpret.com/wp-content/uploads/2017/04/Recipe_logo-1.jpeg'
+      'http://www.verkleedpret.com/wp-content/uploads/2017/04/Recipe_logo-1.jpeg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('Frech Fries', 20)
+      ]
     ), new Recipe(
       'Another Test Recipe',
       'This is a simply a test',
-      'http://www.verkleedpret.com/wp-content/uploads/2017/04/Recipe_logo-1.jpeg'
+      'http://www.verkleedpret.com/wp-content/uploads/2017/04/Recipe_logo-1.jpeg',
+      [
+        new Ingredient('Buns', 3),
+        new Ingredient('Meat', 2)
+      ]
     )];
 
-    getRecipes() {
-      return this.recipes.slice();
-    }
+  getRecipes() {
+    return this.recipes.slice();
+  }
 }
