7883421a2da7cd72987496999847094a22226c26
evuz <jesus_jgb_45@hotmail.com>
2017-08-23 10:02:13 +0200

    Passing Ingredients from Recipes to the Shopping List via a Service

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,29 +1,36 @@
-import { EventEmitter, Output } from '@angular/core';
+import { Injectable, EventEmitter, Output } from '@angular/core';
 import { Recipe } from './recipe.model';
 import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
+@Injectable()
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe(
-      'A test Recipe',
-      'This is a simply a test',
-      'http://www.verkleedpret.com/wp-content/uploads/2017/04/Recipe_logo-1.jpeg',
+      'Tasty Schnitzel',
+      'A super-tasty Schnitzel - just awesome!',
+      'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
       [
         new Ingredient('Meat', 1),
-        new Ingredient('Frech Fries', 20)
-      ]
-    ), new Recipe(
-      'Another Test Recipe',
-      'This is a simply a test',
-      'http://www.verkleedpret.com/wp-content/uploads/2017/04/Recipe_logo-1.jpeg',
+        new Ingredient('French Fries', 20)
+      ]),
+    new Recipe('Big Fat Burger',
+      'What else you need to say?',
+      'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg',
       [
-        new Ingredient('Buns', 3),
-        new Ingredient('Meat', 2)
-      ]
-    )];
+        new Ingredient('Buns', 2),
+        new Ingredient('Meat', 1)
+      ])
+  ];
+
+  constructor(private slService: ShoppingListService) { }
 
   getRecipes() {
     return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
   }
 }
