2fb0f0c4a8e4c9cc5cb7c5fff0ae3df4394187bf
evuz <jesus_jgb_45@hotmail.com>
2017-08-25 14:51:50 +0200

    Reactive Syncing HTML and Form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { FormGroup, FormControl } from '@angular/forms';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   genders = ['male', 'female'];
   signUpForm: FormGroup;
 
