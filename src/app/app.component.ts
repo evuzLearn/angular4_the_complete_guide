adfc8e45c96a181320803005deb600ee81f404d7
evuz <jesus_jgb_45@hotmail.com>
2017-08-25 14:24:46 +0200

    Reactive Setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  suggestUserName() {
-    const suggestedName = 'Superuser';
-  }
+  genders = ['male', 'female'];
+  signUpForm: FormGroup;
 }
