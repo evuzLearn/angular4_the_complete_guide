a666ceab61c1c1bf3da6f57d366889a5928af9fc
evuz <jesus_jgb_45@hotmail.com>
2017-08-25 12:12:20 +0200

    Improving the Reactive Service with Observables Subjects

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,8 +1,9 @@
-import { EventEmitter, Output } from '@angular/core';
+import { Subject } from 'rxjs/Subject';
+
 import { Ingredient } from '../shared/ingredient.model';
 
 export class ShoppingListService {
-  @Output() ingredientsChanged = new EventEmitter<Ingredient[]>();
+  ingredientsChanged = new Subject<Ingredient[]>();
 
   ingredients: Ingredient[] = [
     new Ingredient('Apples', 5),
@@ -15,11 +16,11 @@ export class ShoppingListService {
 
   addIngredient(ingredient: Ingredient) {
     this.ingredients.push(ingredient);
-    this.ingredientsChanged.emit(this.getIngredients());
+    this.ingredientsChanged.next(this.getIngredients());
   }
 
   addIngredients(ingredients: Ingredient[]) {
     this.ingredients.push(...ingredients);
-    this.ingredientsChanged.emit(this.getIngredients());
+    this.ingredientsChanged.next(this.getIngredients());
   }
 }
