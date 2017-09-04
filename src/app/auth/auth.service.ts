1e7c91d6eba9112299dd1d50a896c8b7a75f1866
evuz <jesus_jgb_45@hotmail.com>
2017-09-04 10:21:08 +0200

    Sending the Token

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,6 +1,8 @@
 import * as firebase from 'firebase';
 
 export class AuthService {
+  token: string;
+
   signupUser(email: string, password: string) {
     firebase.auth().createUserWithEmailAndPassword(email, password)
       .then()
@@ -9,7 +11,18 @@ export class AuthService {
 
   signinUser(email: string, password: string) {
     firebase.auth().signInWithEmailAndPassword(email, password)
-      .then((res) => console.log(res))
+      .then((res) => {
+        firebase.auth().currentUser.getIdToken()
+          .then((token: string) => {
+            this.token = token;
+          });
+      })
       .catch((err) => console.log(err));
+  }
+
+  getToken() {
+    firebase.auth().currentUser.getIdToken()
+      .then((token: string) => this.token = token);
+    return this.token;
   }
 }
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -3,33 +3,38 @@ import { Http, Response } from '@angular/http';
 import 'rxjs/Rx';
 
 import { RecipeService } from './../recipes/recipe.service';
+import { AuthService } from './../auth/auth.service';
 import { Recipe } from './../recipes/recipe.model';
 
 @Injectable()
 export class DataStorageService {
   constructor(
     private http: Http,
+    private authService: AuthService,
     private recipeService: RecipeService
   ) { }
 
   storeRecipes() {
+    const token = this.authService.getToken();
     const recipes = this.recipeService.getRecipes();
-    return this.http.put('https://udemy-ng-b1342.firebaseio.com/recipes.json', recipes);
+    return this.http.put('https://udemy-ng-b1342.firebaseio.com/recipes.json?auth=' + token,
+      recipes);
   }
 
   getRecipes() {
-    return this.http.get('https://udemy-ng-b1342.firebaseio.com/recipes.json')
-    .map((res: Response) => {
-      const recipes: Recipe[] = res.json();
-      for (const recipe of recipes) {
-        if (recipe['ingredients']) {
-          recipe['ingredientes'] = [];
+    const token = this.authService.getToken();
+    return this.http.get('https://udemy-ng-b1342.firebaseio.com/recipes.json?auth=' + token)
+      .map((res: Response) => {
+        const recipes: Recipe[] = res.json();
+        for (const recipe of recipes) {
+          if (recipe['ingredients']) {
+            recipe['ingredientes'] = [];
+          }
         }
-      }
-      return recipes;
-    }).subscribe((recipes: Recipe[]) => {
-      this.recipeService.setRecipes(recipes);
-    });
+        return recipes;
+      }).subscribe((recipes: Recipe[]) => {
+        this.recipeService.setRecipes(recipes);
+      });
   }
 
 }
